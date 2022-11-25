c53140b9ee39ca5792563eb0ca7f61869c9f914b
direktor3092 <direktor3092@gmail.com>
2022-11-26 02:21:42 +0300

    feat: add arrays

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -573,8 +573,9 @@ function group(array, keySelector, valueSelector) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], (x) => x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  const result = arr.map((value) => childrenSelector(value));
+  return result.flat();
 }
 
 
@@ -590,8 +591,10 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  const result = indexes.reduce((acc, value) => acc[value], arr);
+  // console.log(result);
+  return result;
 }
 
 
@@ -613,8 +616,22 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const string = arr.join('');
+  const average = Math.ceil(string.length / 2);
+
+
+  if (string.length % 2 === 0) {
+    const start = string.slice(0, average);
+    const end = string.slice(average);
+    return (end + start).split('');
+  }
+
+  const start = string.slice(0, average - 1);
+  const middle = string[average - 1];
+  const end = string.slice(average);
+
+  return (end + middle + start).split('');
 }
 
 
